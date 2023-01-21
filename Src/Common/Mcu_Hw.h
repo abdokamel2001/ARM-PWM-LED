3455a3261e08af6f91d0a33033af01b27bde23b7
Abdo Kamel <69877498+abdokamel2001@users.noreply.github.com>
2023-01-21 05:03:07 +0200

    Update Mcu_Hw.h

--- Src/Common/Mcu_Hw.h
+++ Src/Common/Mcu_Hw.h
@@ -25,13 +25,13 @@
 /**********************************************************************************************************************
  *  GLOBAL CONSTANT MACROS
  *********************************************************************************************************************/
-#define CONTROL_SYSTEM_BASE           	0x400FE000
+#define CONTROL_SYSTEM_BASE            	0x400FE000
 #define CORE_PERIPHERALS_BASE           0xE000E000
-#define GPIO_PORTF_BASE			      		  0x40025000
+#define GPIO_PORTF_BASE			      		      0x40025000
 #define PORTF_DATA                      *((volatile uint32*)(GPIO_PORTF_BASE))
-#define PORTF_DIR												*((volatile uint32*)(GPIO_PORTF_BASE+0x400))
-#define PORTF_DEN												*((volatile uint32*)(GPIO_PORTF_BASE+0x51C))
-#define RCGCGPIO 												*((volatile uint32*)(CONTROL_SYSTEM_BASE+0x608))
+#define PORTF_DIR												           *((volatile uint32*)(GPIO_PORTF_BASE+0x400))
+#define PORTF_DEN												           *((volatile uint32*)(GPIO_PORTF_BASE+0x51C))
+#define RCGCGPIO 											           	*((volatile uint32*)(CONTROL_SYSTEM_BASE+0x608))
 #define STCTRL                          *((volatile uint32*)(CORE_PERIPHERALS_BASE+0x010))
 #define STRELOAD                        *((volatile uint32*)(CORE_PERIPHERALS_BASE+0x014))
 #define STCURRENT                       *((volatile uint32*)(CORE_PERIPHERALS_BASE+0x018))
